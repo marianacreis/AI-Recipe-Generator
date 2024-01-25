45c54df454da0717ac65e69b68a0bf1d50612614
marianacreis <146851643+marianacreis@users.noreply.github.com>
2024-01-25 20:08:25 +0000

    updated prompt

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
 
   let ingredients = document.querySelector("#user-ingredients");
   let apiKey = "dd09922bof9dfbd100daf3a044cat44d";
-  let prompt = `Write a recipe for lunch or dinner that uses the following ingredients : ${ingredients.value}. The recipe need to have those ingredients mandatorily, but it can also include more ingredients. Please write the answer using basic HTML.
+  let prompt = `Write a recipe for lunch or dinner that uses the following ingredients : ${ingredients.value}. Please add more ingredients that you think are necessary for the recipe.  Please write the answer using basic HTML.
 The title of the recipe should be in the begining inside a <strong> element. It should be followed by a line break with the ingredients list and each ingredient should be separated by a <br/> element. After it should have a line break and the cooking instructions inside a <p> element. Please do not add anymore informations, besides the name of the dish, the ingredients and the instructions. `;
   let context =
     "You are a chef that can make delicious dishes with any list of ingredients. Your mission is to generate a delicious recipe using the ingredients provided by the user.";
