454fdab66e83c5a4a3dda22ac810746a015f8c33
marianacreis <146851643+marianacreis@users.noreply.github.com>
2024-01-25 19:57:59 +0000

    Improved AI prompt and CSS

--- src/index.js
+++ src/index.js
@@ -12,16 +12,17 @@ function generateRecipe(event) {
 
   let ingredients = document.querySelector("#user-ingredients");
   let apiKey = "dd09922bof9dfbd100daf3a044cat44d";
-  let prompt = `Write a recipe for lunch or dinner that uses the following ingredients : ${ingredients.value}. The recipe need to have those ingredients mandatorily, but it can also include more ingredients. Please write the answer using basic HTML. The title of the recipe should be in the begining inside a <strong> element. It should be followed by the ingredients list and each ingredient should be separated by a <br/> element. After it should have the cooking instructions inside a <p> element. Please do not add anymore informations, besides the name of the dish, the ingredients and the instructions.  `;
+  let prompt = `Write a recipe for lunch or dinner that uses the following ingredients : ${ingredients.value}. The recipe need to have those ingredients mandatorily, but it can also include more ingredients. Please write the answer using basic HTML.
+The title of the recipe should be in the begining inside a <strong> element. It should be followed by a line break with the ingredients list and each ingredient should be separated by a <br/> element. After it should have a line break and the cooking instructions inside a <p> element. Please do not add anymore informations, besides the name of the dish, the ingredients and the instructions. `;
   let context =
     "You are a chef that can make delicious dishes with any list of ingredients. Your mission is to generate a delicious recipe using the ingredients provided by the user.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayRecipe);
-
   let recipeElement = document.querySelector("#recipe-container");
   recipeElement.classList.remove("hidden");
   recipeElement.innerHTML = `<div class = "generating">Generating a recipe with ${ingredients.value}...</div>`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElemet = document.querySelector("#search-form");
