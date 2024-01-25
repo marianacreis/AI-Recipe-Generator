243f256b28d048d9f7ce5fc3e48389b0ceb87a5f
marianacreis <146851643+marianacreis@users.noreply.github.com>
2024-01-25 17:42:53 +0000

    Added the AI AI integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe-container", {
-    strings: "This is a recipe",
+    strings: response.data.answer,
     autoStart: true,
     cursor: null,
     delay: 1,
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let ingredients = document.querySelector("#user-ingredients");
+  let apiKey = "dd09922bof9dfbd100daf3a044cat44d";
+  let prompt = `Write a recipe for lunch or dinner that uses the following ingredients : ${ingredients.value}. The recipe need to have those ingredients mandatorily, but it can also include more ingredients. Please write the answer using basic HTML. The title of the recipe should be in the begining inside a <strong> element. It should be followed by the ingredients list and each ingredient should be separated by a <br/> element. After it should have the cooking instructions inside a <p> element. Please do not add anymore informations, besides the name of the dish, the ingredients and the instructions.  `;
+  let context =
+    "You are a chef that can make delicious dishes with any list of ingredients. Your mission is to generate a delicious recipe using the ingredients provided by the user.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
+  console.log("Generating a recipe");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
 }
 
 let recipeFormElemet = document.querySelector("#search-form");
